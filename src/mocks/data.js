ad2cfea0d069666f6445e0a968ba810a8dbb92bf
dgnyngz <dgnyngz@gmail.com>
2023-05-05 16:42:10 +0300

    mockdata oluşturuldu

---
+++ src/mocks/data.js
@@ -0,0 +1,70 @@
+const data = {
+  headerSection: {
+    mod: {
+      tr: "Koyu Mod",
+      en: "Dark Mode",
+    },
+    dil: {
+      tr: "Türkçe'ye Geç",
+      en: "switch to english",
+    },
+    selam: {
+      tr: "Selam!",
+      en: "Hi!",
+    },
+    tanitim: {
+      tr: "Ben Doğan. Ben bir full-stack geliştiricisiyim. Sağlam ve ölçeklenebilir ön uç ürünleri üretebilirim. Tanışalım!",
+      en: "I’m Doğan. I’m a full-stack developer. I can craft solid and scalable frontend products. Let’s meet!",
+    },
+    yetenekler: {
+      tr: "Yetenekler",
+      en: "Skills",
+    },
+  },
+  profileSection: {
+    baslik: {
+      tr: " Profil",
+      en: " Profile",
+    },
+    bilgi: {
+      tr: "Temel Bilgiler",
+      en: "Basic İnformation",
+    },
+    dogum: {
+      tr: "Doğum Tarihi",
+      en: "Date of birth",
+    },
+    ikamet: {
+      tr: "İkamet Şehri",
+      en: "City of Residence",
+    },
+    egitim: {
+      tr: "Eğitim Durumu",
+      en: "Educational Status",
+    },
+    tercih: {
+      tr: "Tercih Ettiği Rol",
+      en: "Preferred Role",
+    },
+    hakkımda: {
+      tr: "Hakkımda",
+      en: "About Me",
+    },
+    aciklama: {
+      tr: "tr",
+      en: "en",
+    },
+  },
+  projectSection: {
+    baslik: {
+      tr: "Projeler",
+      en: "Projects",
+    },
+  },
+  footerSection: {
+    istek: {
+      tr: "Bir sonraki ürününüz üzerinde birlikte çalışalım.",
+      en: "Let’s work together on your next product.",
+    },
+  },
+};
