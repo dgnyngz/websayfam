82c79b6d6d5b72e5a55859a3e6e766511929934b
dgnyngz <dgnyngz@gmail.com>
2023-05-05 19:16:20 +0300

    v1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-import Header from "./comps/Header";
-
+import { Header } from "./comps/Header";
+import { themeContext } from "./context/themeContext";
+import { languageContext } from "./context/languageContext";
+import { useState } from "react";
 function App() {
+  const [theme, setTheme] = useState("dark");
+  const [language, setLanguage] = useState("tr");
   return (
-    <div>
-      <Header />
+    <div className="App">
+      <themeContext.Provider value={{ theme, setTheme }}>
+        <languageContext.Provider value={{ language, setLanguage }}>
+          <Header />
+        </languageContext.Provider>
+      </themeContext.Provider>
     </div>
   );
 }
--- src/comps/header.js
+++ src/comps/header.js
@@ -1,9 +1,13 @@
+import { useContext } from "react";
 import mockData from "../mocks/Data";
+import { languageContext } from "../context/languageContext";
 
 export const Header = () => {
+  const { language } = useContext(languageContext);
   return (
     <div>
-      <div>{mockData.headerSection.mod.tr}</div>
+      <div>{mockData.headerSection.mode[language]}</div>
+      <div>{mockData.headerSection.dil.tr}</div>
     </div>
   );
 };
