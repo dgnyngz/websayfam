145afb43134b137654c0c3e11f55f925e45a0e74
dgnyngz <dgnyngz@gmail.com>
2023-05-06 00:28:43 +0300

    cssde düzenlemeler

--- src/comps/header.js
+++ src/comps/header.js
@@ -6,12 +6,19 @@ export const Header = () => {
   const { language, data } = useContext(languageContext);
   return (
     <>
-      <div>
-        <div>{data.headerSection.mode[language]}</div>
-        <div>{data.headerSection.dil[language]}</div>
-        <div>{data.headerSection.selam[language]}</div>
-        <div>{data.headerSection.tanitim[language]}</div>
-        <div>{data.headerSection.davet[language]}</div>
+      <div className="header">
+        <div className="mods">
+          <div className="mod">{data.headerSection.mode[language]}</div>
+          <div className="cizgi">
+            <hr />
+          </div>
+          <div className="dil">{data.headerSection.dil[language]}</div>
+        </div>
+        <div className="xx">
+          <div className="selam">{data.headerSection.selam[language]}</div>
+          <div className="tanitim">{data.headerSection.tanitim[language]}</div>
+          <div className="davet">{data.headerSection.davet[language]}</div>
+        </div>
       </div>
       <div>foto</div>
     </>
