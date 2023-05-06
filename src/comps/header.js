2300826dc140b55195f879f707fe106ac622804c
dgnyngz <dgnyngz@gmail.com>
2023-05-06 15:59:29 +0300

    header kısmı düzenlendi fotograf eklenecek

--- src/comps/header.js
+++ src/comps/header.js
@@ -1,7 +1,8 @@
 import { useContext } from "react";
-
+import fotoSvg from "../logolar/fotograf.svg";
+import linkedin from "../logolar/linkedin.svg";
 import { languageContext } from "../context/LanguageContext";
-
+import github from "../logolar/github.svg";
 export const Header = () => {
   const { language, data, setLanguage } = useContext(languageContext);
   function dilDegis() {
@@ -21,13 +22,28 @@ export const Header = () => {
             </button>
           </div>
         </div>
-        <div className="xx">
-          <div className="selam">{data.headerSection.selam[language]}</div>
-          <div className="tanitim">{data.headerSection.tanitim[language]}</div>
-          <div className="davet">{data.headerSection.davet[language]}</div>
+        <div className="header-main">
+          <div className="xx">
+            <div className="selam">{data.headerSection.selam[language]}</div>
+            <div className="tanitim">
+              {data.headerSection.tanitim[language]}
+            </div>
+            <div className="header-icons">
+              <a href="https://www.linkedin.com/in/do%C4%9Fan-yang%C3%B6z-4b9b22219/">
+                <img src={linkedin} width="31px" height="33.6"></img>
+              </a>
+
+              <a href="https://github.com/dgnyngz">
+                <img src={github} width="31px" height="33.6"></img>
+              </a>
+            </div>
+            <div className="davet">{data.headerSection.davet[language]}</div>
+          </div>
+          <div>
+            <img src={fotoSvg}></img>
+          </div>
         </div>
       </div>
-      <div>foto</div>
     </>
   );
 };
