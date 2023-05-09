df184c5202b83de25bd42d32a44e42fb4fbbfdde
dgnyngz <dgnyngz@gmail.com>
2023-05-09 15:49:23 +0300

    desenler eklendi cssde düzenlemeler yapıldı

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
 import { Header } from "./comps/Header";
-
+import no1 from "./logolar/no1.svg";
+import no2 from "./logolar/no2.svg";
+import no3 from "./logolar/no3.svg";
+import no4 from "./logolar/no4.svg";
+import no5 from "./logolar/no5.svg";
+import no6 from "./logolar/no6.svg";
+import no7 from "./logolar/no7.svg";
 import { Skills } from "./comps/Skill";
 import { Profile } from "./comps/Profile";
 import Projects from "./comps/project/Projects";
@@ -38,6 +44,13 @@ function App() {
     <div className="App">
       <themeContext.Provider value={{ theme, setTheme }}>
         <languageContext.Provider value={{ language, setLanguage, data }}>
+          <img className="no1" src={no1} alt=""></img>
+          <img className="no2" src={no2} alt=""></img>
+          <img className="no3" src={no3} alt=""></img>
+          <img className="no4" src={no4} alt=""></img>
+          <img className="no5" src={no5} alt=""></img>
+          <img className="no6" src={no6} alt=""></img>
+          <img className="no7" src={no7} alt=""></img>
           <Header />
           <Skills />
           <Profile />
--- src/comps/profile.js
+++ src/comps/profile.js
@@ -4,39 +4,41 @@ export const Profile = () => {
   const { language, data } = useContext(languageContext);
   return (
     <>
-      <div className="profil-baslik">
-        <h2>{data.profileSection.baslik[language]}</h2>
-      </div>
-      <div className="profile-section">
-        <div className="info">
-          <h2>{data.profileSection.bilgi[language]}</h2>
-          <div className="bilgiler">
-            <div>
-              <p>{data.profileSection.dogum[language]}</p>
-              <p>{data.profileSection.ikamet[language]}</p>
-              <p>{data.profileSection.egitim[language]}</p>
-              <p>{data.profileSection.tercih[language]}</p>
-            </div>
-            <div>
-              <p>11.06.1996</p>
-              <p>Erzurum</p>
-              <p>Atatürk Ünv. İnşaat Mühendisliği,2021</p>
-              <p>FullStack Web Developer</p>
+      <div className="profile-main">
+        <div className="profil-baslik">
+          <h2>{data.profileSection.baslik[language]}</h2>
+        </div>
+        <div className="profile-section">
+          <div className="info">
+            <h2>{data.profileSection.bilgi[language]}</h2>
+            <div className="bilgiler">
+              <div>
+                <p>{data.profileSection.dogum[language]}</p>
+                <p>{data.profileSection.ikamet[language]}</p>
+                <p>{data.profileSection.egitim[language]}</p>
+                <p>{data.profileSection.tercih[language]}</p>
+              </div>
+              <div>
+                <p>11.06.1996</p>
+                <p>Erzurum</p>
+                <p>Atatürk Ünv. İnşaat Mühendisliği,2021</p>
+                <p>FullStack Web Developer</p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="about">
-          <h2>{data.profileSection.hakkimda[language]}</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-            volutpat.
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
-            nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-            volutpat.
-          </p>
+          <div className="about">
+            <h2>{data.profileSection.hakkimda[language]}</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam
+              nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam
+              erat volutpat.
+            </p>
+          </div>
         </div>
       </div>
     </>
